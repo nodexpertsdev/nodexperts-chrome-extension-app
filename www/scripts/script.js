7530b5269427152648724aaaa754deda06a0c9aa
Mukul <mukul1904@gmail.com>
2017-06-09 11:24:16 +0530

    Style changes, new End point added

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,19 +1,78 @@
 var xmlhttp, obj;
-var url = 'http://localhost:3000/getFeed'
+var url = 'http://nodexperts.com/blog/wp-json/wp/v2/news'
+var feeds = [];
+var intervalTime = 1800000; // 30 minutes
 
-$(document).ready(function () {
+function callNXNewsApi() {
+  console.log('NX end point hitting');
   xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-      populatePopup(JSON.parse(xmlhttp.responseText))
+      feeds = JSON.parse(xmlhttp.responseText);
+      populatePopup(feeds.splice(0, 20))
     }
   }
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
+}
 
+$(document).ready(function () {
+
+  callNXNewsApi();
+  setInterval(function () {
+    callNXNewsApi();
+  }, intervalTime);
+
+  $('.category-btn').click('click', function () {
+    toggleDisplay('.category-list');
+  });
+
+  $('#blogs').click('click', function () {
+    var feedsByBlogs = feeds.filter(function (feed) {
+      return feed.type === 'blogs';
+    });
+    populatePopup(feedsByBlogs);
+  });
+
+  $('#packages').click('click', function () {
+    var feedsByBlogs = feeds.filter(function (feed) {
+      return feed.type === 'packages';
+    });
+    populatePopup(feedsByBlogs);
+  });
+
+  $('#workshops').click('click', function () {
+    var feedsByBlogs = feeds.filter(function (feed) {
+      return feed.type === 'workshops';
+    });
+    populatePopup(feedsByBlogs);
+  });
+
+  $('#daily-quotes').click('click', function () {
+    var feedsByBlogs = feeds.filter(function (feed) {
+      return feed.type === 'daily-quotes';
+    });
+    populatePopup(feedsByBlogs);
+  });
+
+  $('#news').click('click', function () {
+    var feedsByBlogs = feeds.filter(function (feed) {
+      return feed.type === 'news';
+    });
+    populatePopup(feedsByBlogs);
+  });
 });
 
+function toggleDisplay(elm) {
+  if ($(elm).css('display') === 'block') {
+    $(elm).css('display', 'none');
+  } else {
+    $(elm).css('display', 'block');
+  }
+}
+
 function populatePopup (data) {
+  $(".feeds").html('');
   for (var i = 0; i < data.length; i++) {
     var item = data[i];
     var divLabel1 = document.createElement('div');
@@ -25,10 +84,12 @@ function populatePopup (data) {
     var feedImage = document.createElement('a');
     var feedTitle = document.createElement('a');
     var feedDesc = document.createElement('p');
+    var feedTime = document.createElement('time');
 
     $(feedTitle).addClass('feed-title');
     $(feedDesc).addClass('feed-desc');
     $(feedImage).addClass('feed-img');
+    $(feedTime).addClass('feed-time');
 
     feedTitle.setAttribute('href', item.url);
     feedTitle.setAttribute('target', '_blank');
@@ -41,11 +102,12 @@ function populatePopup (data) {
       src: img_url,
     }).addClass('feed-img').appendTo(feedImage);
 
-    feedTitle.innerHTML = item.title;
-    feedDesc.innerHTML = item.description;
+    feedTitle.innerHTML = item.title.rendered;
+    feedDesc.innerHTML = item.content.rendered;
+    feedTime.innerHTML = new Date(item.date).toUTCString();
 
     $(divLabel2).append(feedImage);
-    $(divLabel2).append(feedTitle);
+    $(divLabel2).append(feedTitle).append(feedTime);
     $(divLabel2).append(feedDesc).addClass('feed-desc');
 
     $(divLabel1).append(divLabel2);
